36c60b22a0812078364b92608c8e7145d7bc87cc
Uchqun007 <uchqunjonturdiev@gmail.com>
2024-03-25 23:48:46 +0500

    git add .!

--- src/app/(root)/category/[slug]/page.tsx
+++ src/app/(root)/category/[slug]/page.tsx
@@ -4,17 +4,9 @@ import Link from 'next/link';
 import BlogCard from '@/components/blog/blog';
 import { getBlogByCategory } from '../../../../../service/category.service';
 
-export async function getStaticProps({ params }: { params: { slug: string } }) {
+async function Page({ params }: { params: { slug: string } }) {
 	const category = await getBlogByCategory(params.slug);
 
-	return {
-		props: {
-			category,
-		},
-	};
-}
-
-async function Page({ category }: { category: any }) {
 	return (
 		<div className='max-w-6xl mx-auto'>
 			<div className='relative min-h-[30vh] flex items-center justify-end flex-col'>
